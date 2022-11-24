b8b29e1faa86ddca195a747ce670eedb4947171d
duncan1409 <duncansITworld@gmail.com>
2022-11-25 00:15:32 +0900

    20221124

--- 1000_A+B/APlusB.cpp
+++ 1000_A+B/APlusB.cpp
@@ -3,17 +3,9 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
   int A, B;
   cin >> A >> B;
   cout << A + B;
 
   return 0;
-=======
-	int A, B;
-	cin >> A >> B;
-	cout << A + B;
-
-	return 0;
->>>>>>> 350762b2192ddb807aa6998d887e7496472e3865
 }
